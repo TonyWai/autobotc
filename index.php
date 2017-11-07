adbccb3f5ec17dd9b66f0ad0bd9d6f9e2a7218a3
TonyWai <chanachai.vai@gmail.com>
2017-11-07 22:48:24 +0700

    replymessagetype

--- index.php
+++ index.php
@@ -1,2 +1,33 @@
 <?php
+require_once('./vendor/autoload.php');
+
+#Namespce
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
+$channel_token='3LtCAoEO06hmq622dXan5OCA3DKlltrIvzPkGKZWkscKglmY8b2P1inShw5vHwhhU97ibyBdasIeEsHGAeDLNyqlrmgtL4sCayHhw5Q6s+XVSe7UJaYn5OAQDtsoqlqy3NrUX/0Qav4Y/S1hNTP7LQdB04t89/1O/w1cDnyilFU=';
+$channel_secret='dac747867a7c9c0072bdaa1a1030c932';
+
+#Get message from Line API
+$content=file_get_contents('php://input');
+$events=json_decode($content, true);
+
+if(is_null($events['events'])){
+    foreach($events['events'] as $event){
+        if($event['type']='message'){
+            switch($event['message']['type']){
+                case 'text':
+                    $replyToken=$event['replyToken'];
+
+                    $httpClient=new CurlHTTPClient($channel_token);
+                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+                    $textMessageBuilder = new TextMessageBuilder($respMessage);
+                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+                break;
+            }
+        }
+    }
+}
+
 echo 'OK_wingit';
